667fd5eaa64af8d568e00bdf2de9056834af4c59
Jason Siu <jasonsiu1c1326@gmail.com>
2018-08-19 20:48:31 +0000

    todo edit

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,20 @@
 class TodoController < ApplicationController
-  def index
+    def index
+    end
+    def show
+      @todo = Todo.find_by_id(params[:id])
+    end
+    def new
+    end
+    def edit
+  @todo = Todo.find_by_id(params[:id])
+    end
+  def create
+    t = Todo.new
+  t.description = params['description']
+  t.pomodoro_estimate = params['pomodoro_estimate']
+  t.save
+  redirect_to "/todo/show/#{ t.id }"
+    end
 end
-  def show
-  end
-end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
     get 'todo/index', to: 'todo#index'
     get 'todo/show', to: 'todo#show'
+    get 'todo/create', to: 'todo#create'
+    get 'todo/edit/:id', to: 'todo#edit'
+    get 'todo/new', to: 'todo#new'
 end
